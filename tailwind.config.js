e5d41a675597a088b943969c0ca7f81d26c9babf
eddyg2k <42305396+eddyg2k@users.noreply.github.com>
2025-09-29 20:57:26 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  theme: { extend: {} },
+  plugins: []
+};
