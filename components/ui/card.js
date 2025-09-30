608c083c41b4d10b3d4569025bc84bae974e92f1
eddyg2k <42305396+eddyg2k@users.noreply.github.com>
2025-09-29 21:22:55 -0600

    Delete components/ui directory

--- components/ui/card.js
+++
@@ -1,7 +0,0 @@
-export function Card({ children, className = "" }) {
-  return <div className={\`bg-slate-800 p-4 rounded-lg \${className}\`}>{children}</div>;
-}
-
-export function CardContent({ children }) {
-  return <div className="mt-2 text-base leading-relaxed">{children}</div>;
-}
